bae8e7ec67b1bf7a46428ab8835b0f58194c667a
rose parker <parker.rose@intracitygeeks.org>
2017-07-18 23:23:02 +0000

    just to make sure I sent the saved file

--- car-dealer.js
+++ car-dealer.js
@@ -4,18 +4,23 @@ var carDealer = {
     sedan: [20,'$125.00 a day'],
     
     availSportsCars: function(numavail, cost) {
-      console.log(this.sportsCar[0], 'available', this.sportsCar[1]);
-      if(this.sportsCar[0] > 1) {
-          this.sportsCar[0] = "Rental not available";
+            if(this.sportsCar[0] < 1) {
+          return("Sports Car Rental not available");
       }
-      else {numavail = this.sportsCar[0];}
+      else {console.log(this.sportsCar[0], 'available', this.sportsCar[1]);}
     }, 
     availsuv: function(numavail,cost) {
-        console.log(this.suv[0], 'available', this.suv[1]);
-    },
+         if(this.suv[0] < 1) {
+          return("SUV Rental not available");
+      }
+      else {console.log(this.suv[0], 'available', this.suv[1]);}
+    }, 
     availSedan: function(numavail, cost) {
-        console.log(this.sedan[0], 'available', this.sedan[1]);
-    }
+         if(this.sedan[0] < 1) {
+          return("Sedan Car Rental not available");
+      }
+      else {console.log(this.sedan[0], 'available', this.sedan[1]);}
+    }, 
 };
 carDealer.availSportsCars();
 
